f677f76c49cbaadc93c3fa4b5d05b28b5c378efc
Rasel Kibria <rasel5jkibria@gmail.com>
2024-12-06 00:25:39 +0600

    autofac, serilog and ApplicationDbContext Configured

---
+++ InventoryManagement/App_Start/AutofacConfig.cs
@@ -0,0 +1,25 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using InventoryManagement.DbContexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InventoryManagement.App_Start
+{
+    public class AutofacConfig
+    {
+        public static void RegisterDependencies ()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterType<ApplicationDbContext>().AsSelf().InstancePerRequest();
+
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
--- InventoryManagement/Repositories/Repository.cs
+++ InventoryManagement/Repositories/Repository.cs
@@ -1,11 +1,44 @@
-﻿using System;
+﻿using InventoryManagement.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 
 namespace InventoryManagement.Repositories
 {
-    public class Repository
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
     {
+        private readonly string _connectionString;
+        public Repository()
+        {
+            _connectionString = Models.DbConnection.GetConnectionString();
+        }
+
+        public Task AddAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
